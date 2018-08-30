9c20c64de6e6bd0542a919106cfba1e6ec54573f
Andrei Pitsko <andrei.pitsko@gmail.com>
2018-08-30 11:51:49 -0700

    Initial commit, project created

---
+++ RedditTopListReader/RedditTopListReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RedditTopListReader
+//
+//  Created by Andrei Pitsko on 8/30/18.
+//  Copyright © 2018 Andrei Pitsko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
